980b8bb6a8b27eb4bd254602c386588480150aed
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-08-31 23:39:33 +0530

    fix: DBname in settings.py

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'aman',
+        'NAME': 'crm_db',
         'USER': 'root',
         'PASSWORD': 'password123',
         'HOST': 'localhost',
