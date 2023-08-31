aa45a9931ce85f7dc27c67a0444a06ce64dfab22
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-08-31 23:23:59 +0530

    update mysql config

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'elderco',
+        'NAME': 'aman',
         'USER': 'root',
         'PASSWORD': 'password123',
         'HOST': 'localhost',
--- mydb.py
+++ mydb.py
@@ -17,6 +17,6 @@ dataBase = mysql.connector.connect(
 cursorObject = dataBase.cursor()
 
 # Create a database
-cursorObject.execute("CREATE DATABASE elderco")
+cursorObject.execute("CREATE DATABASE aman")
 
 print("All Done!")
